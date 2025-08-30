5b75c6df76bcf465f4c7d55df246105d59332c23
EGAVSIV <137716439+EGAVSIV@users.noreply.github.com>
2025-08-30 17:11:21 +0530

    Update TG_All data panchang.py

--- TG_All data panchang.py
+++ TG_All data panchang.py
@@ -1,5 +1,5 @@
 import swisseph as swe 
-from datetime import datetime
+import datetime
 import requests
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -185,5 +185,6 @@ if __name__ == "__main__":
     # Image
     generate_panchang_image(data, "panchang.png")
     send_telegram_image("panchang.png", caption="🌟 Panchang Snapshot")
+
 
 
